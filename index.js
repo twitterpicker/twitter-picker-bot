4f235cc4393e3c56ba6c93964ab528d85578fcdb
greyonion404 <twitterpicker111@gmail.com>
2022-09-16 23:26:37 +0600

    console request

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ app.all('/webhook/twitter', async (request, response) => {
         // Send a successful response to Twitter
         response.sendStatus(200);
         // Add your logic to process the event
-        console.log('Received a webhook event:', request.body);
+        console.log('Received a webhook event:', request);
         
-        await consumeEvent(request.body)
+        // await consumeEvent(request.body)
     }
 });
 
